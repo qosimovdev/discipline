89f328529cb00fcea919969df7eb872bc7dffa16
qosimovdev <ibrohim.2128382@gmail.com>
2026-07-06 22:43:02 +0500

    first commit

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 function AppRouter() {
   return (
     <Routes>
+      <Route path="*" element={<Dashboard />} />
       <Route path="/" element={<Dashboard />} />
       <Route path="/tasks" element={<div>Tasks</div>} />
       <Route path="/calendar" element={<div>Calendar</div>} />
