457bdadb8120899d5c6b45ab051e713c5c52f658
qosimovdev <ibrohim.2128382@gmail.com>
2026-07-09 19:09:18 +0500

    feat(auth): implement login and register pages with glass UI

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5656/api',
+    // baseURL: 'http://localhost:5656/api',
+    baseURL: "https://discipline-moyx.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     }
